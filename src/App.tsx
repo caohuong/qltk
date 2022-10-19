fb11f797f8315c7a49ddb73e0266ca793ce089a8
caohuong <s_h22220@yahoo.com.vn>
2022-10-19 15:16:40 +0700

    fix au typescrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
 import './scss/App.scss';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import Blank from './pages/Blank';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
